98f0196d13b4cfe0f7580b5a8b250c728ec927c3
larskaesberg <larskaesberg@gmail.com>
2025-07-22 00:42:36 +0200

    feat: add eval

--- train_sft.py
+++ train_sft.py
@@ -28,11 +28,16 @@ training_args = SFTConfig(
     report_to="wandb",
     logging_steps=10,
     save_steps=500,
-    eval_steps=500,
+    eval_steps=100,
     warmup_steps=100,
-    max_steps=10000,
+    max_steps=1000,
     learning_rate=5e-5,
+    per_device_train_batch_size=1,
+    gradient_accumulation_steps=16,
     max_seq_length=4096,
+    remove_unused_columns=False,
+    group_by_length=True,
+    optim="adamw_torch",
 )
 
 model = AutoModelForCausalLM.from_pretrained(model_name)
