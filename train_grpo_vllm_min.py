4737798393ac79e47820703af5a8b3da20cec8b5
larskaesberg <larskaesberg@gmail.com>
2025-09-26 03:01:47 +0200

    fix: model folder

--- train_grpo_vllm_min.py
+++ train_grpo_vllm_min.py
@@ -266,7 +266,7 @@ def main():
     # Resume only if a run id was provided
     trainer.train(resume_from_checkpoint=bool(args.wandb_run_id))
 
-    trainer.save_model(f"./models/sparc_grpo_{args.model.split('/')[-1]}")
+    trainer.save_model(f"./models/SPaRC_GRPO_{args.model.split('/')[-1]}")
 
     if is_main:
         wandb.finish()
